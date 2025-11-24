dcf1b7d93ff2710579cdf41120bd85667d327e57
naman <naman.shah@tatvasoft.com>
2025-11-24 11:19:56 +0000

    Fixed warning

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -5,7 +5,6 @@ pub struct AppConfig {
     pub base_url: String,
     pub api_token: String,
     pub email: String,
-    pub story_points_custom_field_id: String,
 }
 
 impl AppConfig {
@@ -15,7 +14,6 @@ impl AppConfig {
             base_url: std::env::var("JIRA_BASE_URL")?,
             api_token: std::env::var("JIRA_API_TOKEN")?,
             email: std::env::var("JIRA_EMAIL")?,
-            story_points_custom_field_id: std::env::var("JIRA_STORYPOINT_CF")?.parse()?,
         })
     }
 }
--- src/dtos/story_points_payload.rs
+++ src/dtos/story_points_payload.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UpdateStoryPoint{
